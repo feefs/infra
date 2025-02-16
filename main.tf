1087638dd4b484643c288645b3fa489863de1a5f
feefs <feefs@berkeley.edu>
2025-02-15 22:54:41 -0800

    migrate state to gcs backend

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+locals {
+  gcs_backend_bucket = "${var.project_id}-tf-state"
+}
+
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "6.20.0"
     }
+  }
+  backend "gcs" {
+    bucket = local.gcs_backend_bucket
   }
 }
 
@@ -13,5 +20,5 @@ provider "google" {
 
 module "tfstate" {
   source             = "./tfstate"
-  gcs_backend_bucket = "${var.project_id}-tf-state"
+  gcs_backend_bucket = local.gcs_backend_bucket
 }
--- tfstate/main.tf
+++ tfstate/main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "tfstate" {
+resource "google_storage_bucket" "main" {
   name                        = var.gcs_backend_bucket
   location                    = "us-west1"
   uniform_bucket_level_access = true
