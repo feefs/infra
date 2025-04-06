c7922b4693781210e4a61cbc0cc6dd32e82c6955
feefs <feefs@berkeley.edu>
2025-04-05 21:20:15 -0700

    configure api management and add cloud logging api

--- main.tf
+++ main.tf
@@ -18,6 +18,17 @@ provider "google" {
   project = var.project_id
 }
 
+resource "google_project_service" "project" {
+  for_each = toset([
+    "artifactregistry.googleapis.com",
+    "compute.googleapis.com",
+    "storage-component.googleapis.com",
+    "logging.googleapis.com",
+  ])
+  service            = each.key
+  disable_on_destroy = false
+}
+
 module "tf-state" {
   source             = "./tf-state"
   gcs_backend_bucket = local.gcs_backend_bucket
