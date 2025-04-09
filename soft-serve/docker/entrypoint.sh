d39e14272bfb29ba7860bf8ab7eb8b138f7bc2ec
feefs <feefs@berkeley.edu>
2025-04-08 23:23:07 -0700

    set gcsfuse --file-mode to 744 so generated server-side hooks are executable

--- soft-serve/docker/entrypoint.sh
+++ soft-serve/docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
-gcsfuse --app-name=soft-serve ${GCSFUSE_BUCKET} /soft-serve
+gcsfuse \
+  --app-name=soft-serve \
+  --file-mode=744 \
+  ${GCSFUSE_BUCKET} /soft-serve
 mount | grep gcsfuse
 cp /root/config.yaml /soft-serve
 NO_COLOR=1 exec /usr/local/bin/soft serve 2>&1
