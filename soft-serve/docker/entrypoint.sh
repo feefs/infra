a2901dbfc399b086962d06d09d44745076752bf4
feefs <feefs@berkeley.edu>
2025-04-13 22:27:19 -0700

    use parallel to tail gcsfuse logs

--- soft-serve/docker/entrypoint.sh
+++ soft-serve/docker/entrypoint.sh
@@ -3,7 +3,10 @@ set -euxo pipefail
 gcsfuse \
   --app-name=soft-serve \
   --file-mode=744 \
+  --log-file=/tmp/gcsfuse-logs.log \
   ${GCSFUSE_BUCKET} /soft-serve
 mount | grep gcsfuse
 cp /root/config.yaml /soft-serve
-NO_COLOR=1 exec /usr/local/bin/soft serve 2>&1
+parallel --no-notice --halt soon,fail=1 --line-buffer ::: \
+  "tail -f /tmp/gcsfuse-logs.log" \
+  "NO_COLOR=1 SOFT_SERVE_DEBUG=1 SOFT_SERVE_VERBOSE=1 /usr/local/bin/soft serve 2>&1"
